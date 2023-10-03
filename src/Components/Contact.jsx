26f450410f732174a2225bde98b37a703aee360b
TomShep998 <tomshep998@outlook.com>
2023-10-03 15:19:45 +0100

    Navbar updated, Contact update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -10,15 +10,15 @@ const Contact = () => {
                 <label htmlFor="email" className="text-white">
                 Your Email
                 </label>
-                <input type="email" id="email" required placeholder="example@example.com" className="rounded bg-transparent" />
+                <input type="email" id="email" required placeholder="example@example.com" className="rounded bg-[#27272a]" />
                 <label htmlFor="subject" className="text-white">
                 Subject
                 </label>
-                <input type="text" id="subject" required placeholder="Subject" className="rounded bg-transparent border-cyan-500 border-2 border-solid text-white" />
+                <input type="text" id="subject" required placeholder="Subject" className="rounded bg-[#27272a]  text-white " />
                 <label htmlFor="message" className="text-white">
                 Your Message
                 </label>
-                <input type="text" id="message" required placeholder="Enter your message here" className="h-[300px] rounded bg-transparent" />
+                <input type="text" id="message" required placeholder="Enter your message here" className="h-[150px] rounded bg-[#27272a]" />
       
 
             </form>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NavLink from './NavLink'
 import {Outlet, Route, Routes} from 'react-router-dom'
 const Navbar = () =>{
-  const NavLinkData=  [{
+  const navLinkData=  [{
         id:0,
         path:'#Home',
         title:'Home',
@@ -23,9 +23,16 @@ const Navbar = () =>{
 
     return(
         <>
-        <nav className='w-100 h-[50px][#212626] fixed top-0 right-0 left-0'>
-            <ul className='flex flex-row list-none text-white'>
-              
+        <nav className='w-100 h-[50px] bg-[#27272a] opacity-40 fixed top-0 right-0 left-0 z-10'>
+            <ul className='flex flex-row list-none text-white mt-2 justify-end'>
+                    {
+                        navLinkData.map((link) => (
+                        <li className="text-lg font-semibold px-6 " key={link.id}>
+                        <NavLink title={link.title} path={link.path} />
+                        </li>
+                        ))
+                    }
+
             </ul>
         </nav>
         <Outlet />
